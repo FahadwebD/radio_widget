a6bec7e961d82e0383880f4415256269d6145631
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-19 23:58:14 +0600

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import { Accordion, Card, useAccordionButton } from 'react-bootstrap';
+
+
+
+// function CustomToggle({ children, eventKey }) {
+//   const decoratedOnClick = useAccordionButton(eventKey, () =>
+//     console.log('totally custom!'),
+//   );
+
+//   return (
+//     <button
+//       type="button"
+//       style={{ backgroundColor: 'pink' }}
+//       onClick={decoratedOnClick}
+//     >
+//       {children}
+//     </button>
+//   );
+// }
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Accordion defaultActiveKey="0">
+      <Card>
+
+      <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+        <Card.Header>
+          <CustomToggle eventKey="0">Click me!</CustomToggle>
+        </Card.Header>
+       
+      </Card>
+      <Card>
+        <Card.Header>
+          <CustomToggle eventKey="1">Click me!</CustomToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>Hello! I'm another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion> */}
     </div>
   );
 }
